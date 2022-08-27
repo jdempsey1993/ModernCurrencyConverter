747e2944e852a27b0761e4177e14dbdd3a56b9d7
Jack Dempsey <jackdempsey@hey.com>
2022-08-27 17:05:22 -0400

    Update main.js
    Update to main.js to declare variables

---
+++ main.js
@@ -0,0 +1,10 @@
+//Define const for currency converter and parameters
+
+const initialValue = 100
+const initialCurrency = "USD"
+const conversionCurrency = "EUR"
+const rateOfConversion = 1
+const endAmount = initialValue*rateOfConversion+conversionCurrency
+
+console.log(endAmount)
+// Define currency convert function
