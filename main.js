03f5d606673ccfd86c6f9366e03933a84ccfaa1c
Jack Dempsey <jackdempsey@hey.com>
2022-08-27 15:07:24 -0400

    Update main.js
    Linked Currency Converter API

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("This is working.")
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0850f5fa23msh32a1da48681f49bp1e14a8jsn298ebe3bcdc3',
+		'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+	}
+};
+
+fetch('https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=EUR&amount=1&language=en', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
