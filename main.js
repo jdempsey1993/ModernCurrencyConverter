4e84192a37dd907aeb06b0df188b64f8ef9db7df
Jack Dempsey <jackdempsey@hey.com>
2022-08-29 20:59:16 -0400

    Update to index.html
    Updated index.html inputs.

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-//Define const for currency converter and parameters
-
-const initialValue = 100
-const initialCurrency = "USD"
-const conversionCurrency = "EUR"
-const rateOfConversion = 1
-const endAmount = initialValue*rateOfConversion+conversionCurrency
-
-console.log(endAmount)
-// Define currency convert function
+const currencyConverter = (initalVal,initialCur,conversionCur,conversionRate) => {
+    const endVal = initalVal*conversionRate
+}
