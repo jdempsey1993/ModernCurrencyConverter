fe42c1c784c51f44a7c9c31e0f659e0a75b96ca7
Jack Dempsey <jackdempsey@hey.com>
2022-08-29 21:26:27 -0400

    Update main.js , index.html
    Initial function to do math to convert initial value and switch statement to account for currency conventions
    
    Updated index.html with more currency types

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-const currencyConverter = (initalVal,initialCur,conversionCur,conversionRate) => {
-    const endVal = initalVal*conversionRate
+function convertCurrency (intVal,intCur,secCur,exchangeRate){
+    let symbolLeft=""
+    let symbolRight=""
+    let delimiter="."  
+        switch(secCur){
+            case "USD":
+                symbolLeft="$"
+                break
+            case "EUR":
+                symbolLeft="€"
+                break
+            case "GBP":
+                symbolLeft="£"
+                break
+            case "SEK":
+                symbolRight="kr"
+                break
+            case "NOK":
+                symbolRight="kr"
+                break
+            case "DKK":
+                symbolRight="kr"
+                break
+                
+        }
+
+        return symbolLeft + ((intVal*exchangeRate).toString().replace(".",delimiter)+symbolRight)
 }
+
+console.log(convertCurrency(100,"USD","EUR",1.02))
